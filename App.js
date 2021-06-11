1f6c5e8bc2e2e4ec39beab7a468827bb42875389
JI XIANG <jixiangpro@JIs-MacBook-Pro.local>
2021-06-11 15:28:57 +0800

    Added auto refresh using interval

--- App.js
+++ App.js
@@ -13,16 +13,30 @@ const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
+  const [displayTime, setDisplayTime] = useState("");
 
   useEffect(() => {
     loadBusstopData();
+
+    const interval = setInterval(loadBusstopData, 10000); //re-run the code at every 10000ms = 10s
+
+    return () => clearInterval(interval); //un-mount then you will clear the interval so that it stops running
   }, []); //so this hook wil run once when it is initialise since the second parameter is a null array.
 
   async function loadBusstopData() {
+    setLoading(true);
+
     //using async and await method
     const response = await fetch(BUSSTOP_URL); //fetch data from the URL stated.
     const responseData = await response.json();
-    console.log(responseData);
+    const busData = responseData.services.filter(
+      //filter the array
+      (service) => service.no === "150" //get back results that the no="150"
+    )[0];
+
+    setDisplayTime(busData.next.time);
+    setLoading(false); //so that the app is loaded based on the if conditions written below
+    console.log(busData);
 
     //useing .then method
     // .then((response) => {
@@ -38,9 +52,9 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.title}>Bus arrival time:</Text>
       <Text style={styles.time}>
-        {loading ? <ActivityIndicator size="large" /> : "Loaded"}
+        {loading ? <ActivityIndicator size="large" /> : displayTime}
       </Text>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={loadBusstopData} style={styles.button}>
         <Text style={styles.buttonText}>Refresh!</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
